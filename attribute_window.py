7869b73f88b25886144d9031f53ee3320bf92196
user <user@user.use>
2026-05-23 09:20:37 -0300

    feat: add dock toolbar with toggle editing, zoom and flash actions
    - Toggle editing acts on the layer of the selected tree item
    - Button state synced via editingStarted/editingStopped signals
    - Action menu (duplicate feature) and buttons (cancel, ok) suppressed on all feature forms

--- attribute_window.py
+++ attribute_window.py
@@ -4,14 +4,14 @@ import os
 
 from qgis.PyQt.QtCore import QCoreApplication, QSettings, QSize, Qt, QTimer, QTranslator
 from qgis.PyQt.QtGui import QColor, QIcon, QStandardItem, QStandardItemModel
-from qgis.PyQt.QtWidgets import QMenu, QScrollArea, QSplitter, QTreeView
+from qgis.PyQt.QtWidgets import QAction, QDialogButtonBox, QMenu, QScrollArea, QSplitter, QTreeView
 
 try:
     from qgis.PyQt.QtGui import QAction  # Qt6
 except ImportError:
     from qgis.PyQt.QtWidgets import QAction  # Qt5
 
-from qgis.core import QgsProject, QgsSettings, QgsVectorLayer, edit
+from qgis.core import QgsApplication, QgsProject, QgsSettings, QgsVectorLayer, edit
 from qgis.gui import QgsHighlight
 
 from .attribute_window_dockwidget import AttributeWindowDockWidget
@@ -31,15 +31,9 @@ class AttributeWindow:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: QGIS interface instance
-        :type iface: QgsInterface
-        """
         self.iface = iface
         self.plugin_dir = os.path.dirname(__file__)
 
-        # initialize locale
         locale_val = QSettings().value("locale/userLocale", "en")
         locale = str(locale_val)[0:2]
         locale_path = os.path.join(self.plugin_dir, "i18n", f"AttributeWindow_{locale}.qm")
@@ -49,13 +43,14 @@ class AttributeWindow:
             self.translator.load(locale_path)
             QCoreApplication.installTranslator(self.translator)
 
-        # Declare instance attributes
         self.actions = []
         self.menu = self.tr("&Feature Attribute Window")
         self.toolbar = self.iface.addToolBar("AttributeWindow")
         self.toolbar.setObjectName("AttributeWindow")
 
         self.dockwidget = None
+        self.toggleEditingAction = None
+        self._editingTrackedLayer = None
 
         self.featureForm = None
         self.formScrollArea = None
@@ -70,7 +65,6 @@ class AttributeWindow:
         self.timer.timeout.connect(self.finishFlash)
 
     def tr(self, message):
-        """Get the translation for a string using Qt translation API."""
         return QCoreApplication.translate("AttributeWindow", message)
 
     def add_action(
@@ -83,7 +77,6 @@ class AttributeWindow:
         whats_this=None,
         parent=None,
     ):
-        """Add a toolbar icon to the toolbar."""
         icon = QIcon(os.path.join(os.path.dirname(__file__), "icon.png"))
         action = QAction(icon, text, parent)
         action.setCheckable(True)
@@ -111,20 +104,106 @@ class AttributeWindow:
         )
 
         self.dockwidget = AttributeWindowDockWidget()
-        self.dockwidget.setMinimumSize(QSize(200, 300))
         self.dockwidget.setAllowedAreas(
             Qt.DockWidgetArea.LeftDockWidgetArea | Qt.DockWidgetArea.RightDockWidgetArea
         )
         self.dockwidget.setToggleVisibilityAction(self.actions[0])
         self.iface.addDockWidget(_RightDockWidgetArea, self.dockwidget)
+
+        self.toggleEditingAction = QAction(
+            QgsApplication.getThemeIcon("mActionToggleEditing.svg"),
+            self.tr("Toggle Editing"),
+            self.dockwidget,
+        )
+        self.toggleEditingAction.setCheckable(True)
+        self.toggleEditingAction.setEnabled(False)
+        self.toggleEditingAction.triggered.connect(self._toggleEditing)
+        self.dockwidget.toolbar.addAction(self.toggleEditingAction)
+
+        zoomAction = QAction(
+            QgsApplication.getThemeIcon("mActionZoomToSelected.svg"),
+            self.tr("Zoom to Feature"),
+            self.dockwidget,
+        )
+        zoomAction.triggered.connect(self.zoomToFeatureActionFunc)
+        self.dockwidget.toolbar.addAction(zoomAction)
+
+        flashAction = QAction(
+            QIcon(os.path.join(self.plugin_dir, "icons", "mActionFlash.svg")),
+            self.tr("Flash Feature"),
+            self.dockwidget,
+        )
+        flashAction.triggered.connect(self.flashFeatureActionFunc)
+        self.dockwidget.toolbar.addAction(flashAction)
+
         self.updateAttributes()
         self.dockwidget.hide()
 
         self.iface.mapCanvas().selectionChanged.connect(self.updateAttributes)
 
+    def _currentLayer(self):
+        """Return the layer of the currently selected tree item, or None."""
+        if self.a is None:
+            return None
+        try:
+            idx = self.featuresInLayerTree.index(self.a)
+            return self.featuresInLayerTree[idx + 2]
+        except (ValueError, IndexError):
+            return None
+
+    def _toggleEditing(self):
+        """Toggle editing on the layer of the selected tree item."""
+        layer = self._currentLayer()
+        if layer is None or not isinstance(layer, QgsVectorLayer):
+            self.toggleEditingAction.setChecked(False)
+            return
+        if layer.isEditable():
+            layer.commitChanges()
+        else:
+            layer.startEditing()
+        self._syncToggleEditingButton()
+
+    def _trackEditingLayer(self, layer):
+        """Switch editing-state signal tracking to a new layer."""
+        if self._editingTrackedLayer is not None:
+            try:
+                self._editingTrackedLayer.editingStarted.disconnect(
+                    self._syncToggleEditingButton
+                )
+                self._editingTrackedLayer.editingStopped.disconnect(
+                    self._syncToggleEditingButton
+                )
+            except Exception:
+                pass
+
+        self._editingTrackedLayer = layer
+
+        if layer is not None and isinstance(layer, QgsVectorLayer):
+            layer.editingStarted.connect(self._syncToggleEditingButton)
+            layer.editingStopped.connect(self._syncToggleEditingButton)
+
+        self._syncToggleEditingButton()
+
+    def _syncToggleEditingButton(self):
+        """Sync the toggle editing button with the tracked layer's editing state."""
+        if self.toggleEditingAction is None:
+            return
+        layer = self._currentLayer()
+        if layer is not None and isinstance(layer, QgsVectorLayer):
+            self.toggleEditingAction.setEnabled(True)
+            self.toggleEditingAction.setChecked(layer.isEditable())
+        else:
+            self.toggleEditingAction.setEnabled(False)
+            self.toggleEditingAction.setChecked(False)
+
+    def _suppressActionMenu(self, form):
+        """Oculta o menu de ações (duplicar recurso) do QgsFeatureForm."""
+        for child in form.findChildren(QDialogButtonBox):
+            child.hide()
+
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
         self.iface.mapCanvas().selectionChanged.disconnect(self.updateAttributes)
+        self._trackEditingLayer(None)
 
         for action in self.actions:
             self.iface.removePluginMenu(self.tr("&Feature Attribute Window"), action)
@@ -132,8 +211,6 @@ class AttributeWindow:
         del self.toolbar
 
     def _wrapInScrollArea(self, widget):
-        """Wrap a widget in a QScrollArea to prevent layout blowup
-        with tall Drag-and-Drop Designer forms."""
         scroll = QScrollArea()
         scroll.setWidgetResizable(True)
         try:
@@ -144,7 +221,6 @@ class AttributeWindow:
         return scroll
 
     def _removeOldForm(self):
-        """Clean up the previous feature form and its scroll area."""
         if self.featureForm is not None:
             try:
                 self.featureForm.accept()
@@ -176,11 +252,13 @@ class AttributeWindow:
         self._removeOldForm()
 
         self.featuresInLayerTree = []
+        self.a = None
 
         if self.dockwidget is not None:
-            self.dockwidget.setWidget(self.splitter)
+            self.dockwidget.setContentWidget(self.splitter)
 
         if self.iface.mapCanvas().currentLayer() is None:
+            self._trackEditingLayer(None)
             return
 
         for layer in self.iface.mapCanvas().layers():
@@ -219,15 +297,21 @@ class AttributeWindow:
             first_layer = self.featuresInLayerTree[2]
             try:
                 self.featureForm = self.iface.getFeatureForm(first_layer, first_feature)
+                self._suppressActionMenu(self.featureForm)
                 self.formScrollArea = self._wrapInScrollArea(self.featureForm)
                 self.splitter.addWidget(self.formScrollArea)
                 self.featureForm.show()
                 self.splitter.setSizes([100, 500])
                 if self.dockwidget is not None:
-                    self.dockwidget.setWidget(self.splitter)
+                    self.dockwidget.setContentWidget(self.splitter)
+                self.a = self.featuresInLayerTree[0]
+                self._trackEditingLayer(first_layer)
             except Exception:
                 self.featureForm = None
                 self.formScrollArea = None
+                self._trackEditingLayer(None)
+        else:
+            self._trackEditingLayer(None)
 
     def updateFeatureFromTreeView(self, index):
         if not index.isValid():
@@ -245,10 +329,12 @@ class AttributeWindow:
                 self._removeOldForm()
 
                 self.featureForm = self.iface.getFeatureForm(layer, feature)
+                self._suppressActionMenu(self.featureForm)
                 self.formScrollArea = self._wrapInScrollArea(self.featureForm)
                 self.splitter.addWidget(self.formScrollArea)
                 self.featureForm.show()
                 self.splitter.setSizes([100, 500])
+                self._trackEditingLayer(layer)
                 return
 
     def openMenu(self, position):
