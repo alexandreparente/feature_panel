67e8ad0fb2e2b71256fdb9a7b7d32500ab43fdc6
user <user@user.use>
2026-05-22 22:31:05 -0300

    refactor: replace QDockWidget with QgsDockWidget
    Remove custom closingPlugin signal and closeEvent, now handled natively by QgsDockWidget

--- attribute_window_dockwidget.py
+++ attribute_window_dockwidget.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
-
-from qgis.PyQt.QtCore import pyqtSignal
-from qgis.PyQt.QtWidgets import QDockWidget
 from qgis.PyQt import uic
+from qgis.gui import QgsDockWidget
 
 FORM_CLASS, _ = uic.loadUiType(
     os.path.join(os.path.dirname(__file__), "attribute_window_dockwidget_base.ui")
 )
 
-
-class AttributeWindowDockWidget(QDockWidget, FORM_CLASS):
-    closingPlugin = pyqtSignal()
-
+class AttributeWindowDockWidget(QgsDockWidget, FORM_CLASS):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-
-    def closeEvent(self, event):
-        self.closingPlugin.emit()
-        event.accept()
