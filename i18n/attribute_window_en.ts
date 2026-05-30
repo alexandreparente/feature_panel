0c26cc2f6b941b5be795858ed2679989ffaabec8
user <user@user.use>
2026-05-30 09:03:27 -0300

    feat: add translation support

---
+++ i18n/attribute_window_en.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>AttributeWindow</name>
+    <message>
+        <location filename="../attribute_window.py" line="178"/>
+        <source>&amp;Feature Attribute Window</source>
+        <translation>&amp;Feature Attribute Window</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="100"/>
+        <source>Toggle Editing</source>
+        <translation>Toggle Editing</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="110"/>
+        <source>Edit All Selected Features</source>
+        <translation>Edit All Selected Features</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="540"/>
+        <source>Zoom to Feature</source>
+        <translation>Zoom to Feature</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="128"/>
+        <source>Flash Feature</source>
+        <translation>Flash Feature</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="136"/>
+        <source>Delete Feature</source>
+        <translation>Delete Feature</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="539"/>
+        <source>Deselect</source>
+        <translation>Deselect</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="541"/>
+        <source>Pan to Feature</source>
+        <translation>Pan to Feature</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="542"/>
+        <source>Flash</source>
+        <translation>Flash</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="544"/>
+        <source>Delete</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="34"/>
+        <source>&amp;Feature Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="87"/>
+        <source>Feature Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/attribute_window_pt_BR.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>AttributeWindow</name>
+    <message>
+        <location filename="../attribute_window.py" line="178"/>
+        <source>&amp;Feature Attribute Window</source>
+        <translation>&amp;Janela de Atributos</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="100"/>
+        <source>Toggle Editing</source>
+        <translation>Alternar Edição</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="110"/>
+        <source>Edit All Selected Features</source>
+        <translation>Editar Todas as Feições Selecionadas</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="540"/>
+        <source>Zoom to Feature</source>
+        <translation>Zoom para Feição</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="128"/>
+        <source>Flash Feature</source>
+        <translation>Destacar Feição</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="136"/>
+        <source>Delete Feature</source>
+        <translation>Excluir Feição</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="539"/>
+        <source>Deselect</source>
+        <translation>Desselecionar</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="541"/>
+        <source>Pan to Feature</source>
+        <translation>Mover para Feição</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="542"/>
+        <source>Flash</source>
+        <translation>Destacar</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="544"/>
+        <source>Delete</source>
+        <translation>Excluir</translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="34"/>
+        <source>&amp;Feature Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../attribute_window.py" line="87"/>
+        <source>Feature Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
